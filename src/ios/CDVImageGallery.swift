684f9f9b4b332e5c0d1faf5774ac7c0ab62376d9
Tom Bassett <tbassett44@gmail.com>
2019-06-06 15:31:34 -0600

    fix ios directory

--- src/ios/CDVImageGallery.swift
+++ src/ios/CDVImageGallery.swift
@@ -90,7 +90,7 @@ import AVKit
         let url = NSURL(string: NSTemporaryDirectory())
         let imagePath = url!.appendingPathComponent(imageName)
         let urlString: String = imagePath!.absoluteString
-        let imageData = UIImageJPEGRepresentation(image, CGFloat(self.quality ?? 1.0))
+        let imageData = UIImageJPEGRepresentation(image, CGFloat(self.args.quality ?? 1.0))
         fileManager.createFile(atPath: urlString as String, contents: imageData, attributes: nil);
         return urlString;
     }
