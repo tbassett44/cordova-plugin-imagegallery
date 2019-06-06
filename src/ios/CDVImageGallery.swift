ed2ee1fd417d23acfd4d340d0d9ccf4af49d2e02
Tom Bassett <tbassett44@gmail.com>
2019-06-06 14:04:53 -0600

    additional settings and using temp directory

--- src/ios/CDVImageGallery.swift
+++ src/ios/CDVImageGallery.swift
@@ -21,65 +21,76 @@ import AVKit
     @objc(show:)
     func show(_ command: CDVInvokedUrlCommand) {
         returncommand=command;
-      let gallery = GalleryController()
+        let gallery = GalleryController()
         gallery.delegate = self
         //configure settings!
         args=CDVImageGalleryOptions();
+        args.mode=(command.arguments[0] as AnyObject).value(forKey: "mode") as! String
         args.maxImages=(command.arguments[0] as AnyObject).value(forKey: "maximumImagesCount") as! Int
+        args.quality=((command.arguments[0] as AnyObject).value(forKey: "quality") as! Float)/100
         Config.Camera.imageLimit = args.maxImages
         print(args)
         if(args.mode=="LibraryOnly"){
             Config.tabsToShow = [.imageTab]
+        }else if(args.mode=="LibraryAndCamera"){
+            Config.tabsToShow = [.imageTab,.cameraTab]
+        }else if(args.mode=="CameraOnly"){
+            Config.tabsToShow = [.cameraTab]
+        }else if(args.mode=="VideoOnly"){
+            Config.tabsToShow = [.videoTab]
+        }else if(args.mode=="AllMedia"){
+            Config.tabsToShow = [.imageTab,.cameraTab,.videoTab]
+        }else{
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_ERROR,
+                messageAs: "Invalid Mode"
+            )
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: self.returncommand.callbackId
+            )
+            return
         }
         self.viewController.present(gallery, animated: true, completion: nil)
-  }
-  func galleryControllerDidCancel(_ controller: GalleryController) {
-      controller.dismiss(animated: true, completion: nil)
-      gallery = nil
-    let pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_ERROR,
-        messageAs: "Closed"
-    )
-    self.commandDelegate!.send(
-        pluginResult,
-        callbackId: self.returncommand.callbackId
-    )
-  }
-
-  func galleryController(_ controller: GalleryController, didSelectVideo video: Video) {
-      controller.dismiss(animated: true, completion: nil)
-      gallery = nil
-      
-      
-//      editor.edit(video: video) { (editedVideo: Video?, tempPath: URL?) in
-//          if let tempPath = tempPath {
-//              let controller = AVPlayerViewController()
-//              controller.player = AVPlayer(url: tempPath)
-//
-//              self.viewController.present(controller, animated: true, completion: nil)
-//          }
-//      }
-  }
+    }
+    func galleryControllerDidCancel(_ controller: GalleryController) {
+        controller.dismiss(animated: true, completion: nil)
+        gallery = nil
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR,
+            messageAs: "Closed"
+        )
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: self.returncommand.callbackId
+        )
+    }
+    
+    func galleryController(_ controller: GalleryController, didSelectVideo video: Video) {
+        controller.dismiss(animated: true, completion: nil)
+        gallery = nil
+        
+        
+        //      editor.edit(video: video) { (editedVideo: Video?, tempPath: URL?) in
+        //          if let tempPath = tempPath {
+        //              let controller = AVPlayerViewController()
+        //              controller.player = AVPlayer(url: tempPath)
+        //
+        //              self.viewController.present(controller, animated: true, completion: nil)
+        //          }
+        //      }
+    }
     func galleryController(_ controller: GalleryController, requestLightbox images: [Image]) {
         controller.dismiss(animated: true, completion: nil)
         gallery = nil
     }
-    func getDirectoryPath() -> NSURL {
-        let path = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent("temp")
-        let url = NSURL(string: path)
-        return url!
-    }
+    
     func saveImageDocumentDirectory(image: UIImage, imageName: String) -> String{
         let fileManager = FileManager.default
-        let path = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent("temp")
-        if !fileManager.fileExists(atPath: path) {
-            try! fileManager.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
-        }
-        let url = NSURL(string: path)
+        let url = NSURL(string: NSTemporaryDirectory())
         let imagePath = url!.appendingPathComponent(imageName)
         let urlString: String = imagePath!.absoluteString
         let imageData = UIImageJPEGRepresentation(image, CGFloat(self.quality ?? 1.0))
-        //let imageData = UIImagePNGRepresentation(image)
         fileManager.createFile(atPath: urlString as String, contents: imageData, attributes: nil);
         return urlString;
     }
@@ -98,10 +109,10 @@ import AVKit
         
         return randomString
     }
-  func galleryController(_ controller: GalleryController, didSelectImages images: [Image]) {
-    //return images!
-    controller.dismiss(animated: true, completion: nil)
-    let thisitem=self;
+    func galleryController(_ controller: GalleryController, didSelectImages images: [Image]) {
+        //return images!
+        controller.dismiss(animated: true, completion: nil)
+        let thisitem=self;
         Image.resolve(images: images, completion: { [weak self] resolvedImages in
             let returnimages=resolvedImages.compactMap({ $0 })
             var imagelist=[] as [String]
@@ -117,5 +128,5 @@ import AVKit
                 callbackId: thisitem.returncommand.callbackId
             )
         })
-  }
+    }
 }
--- www/cordova-plugin-imagegallery.js
+++ www/cordova-plugin-imagegallery.js
@@ -4,10 +4,11 @@ exports.show=function(success,error,options){
 		options = {};
 	}
 	var params = {
+		mode:(options.mode)?options.mode:'LibraryOnly',
 		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
 		width: 0,//legacy, not supported in ios
 		height:0,//legacy, not supported in ios
-		quality: options.quality ? options.quality : 100	
+		quality: options.quality ? options.quality : 100
 	};
 	exec(success, error, "CDVImageGallery", "show", [params]);
 }
