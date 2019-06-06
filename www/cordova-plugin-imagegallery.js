0be671528ec3c975ddb7fac6d93e887b6c8b32e5
Tom Bassett <tbassett44@gmail.com>
2019-06-05 21:01:30 -0600

    updates

--- www/cordova-plugin-imagegallery.js
+++ www/cordova-plugin-imagegallery.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.show = function(arg0, success, error) {
-    exec(success, error, "cordova-plugin-imagegallery", "show", [arg0]);
+    exec(success, error, "CDVImageGallery", "show", [arg0]);
 };
