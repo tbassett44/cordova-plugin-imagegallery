a8f25e2c154a8c6a083697ff8bd37272546a70e6
Tom Bassett <tbassett44@gmail.com>
2019-10-28 10:01:31 -0600

    fix android permissions

--- www/cordova-plugin-imagegallery.js
+++ www/cordova-plugin-imagegallery.js
@@ -1,11 +1,16 @@
 var exec = require('cordova/exec');
+exports.hasReadPermission = function(callback) {
+  return exec(callback, null, "CDVImageGallery", "hasReadPermission", []);
+};
+exports.requestReadPermission = function(callback, failureCallback) {
+  return exec(callback, failureCallback, "CDVImageGallery", "requestReadPermission", []);
+};
 exports.show=function(success,error,options){
 	if (!options) {
 		options = {};
 	}
 	var params = {
 		mode:(options.mode)?options.mode:'LibraryOnly',
-		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
         gridSize: options.gridSize ? options.gridSize : 3,
         cellSpacing: options.cellSpacing ? options.cellSpacing : 2,
         maxDuration: options.maxDuration ? options.maxDuration : 2,
